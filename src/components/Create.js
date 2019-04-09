a1b40310b006100920b64769f2d5d534eb1881ce
PrzemekGdula <przemo.g12@wp.pl>
2019-04-09 10:06:24 +0200

    form with handle submit

--- src/components/Create.js
+++ src/components/Create.js
@@ -7,20 +7,33 @@ class Create extends Component {
         age: ''
     }
 
-    handleChange = (event, fieldName) => {
-        this.setState({ [fieldName]: event.target.value });
+    // handleChange = (event, fieldName) => {
+    // this.setState({ [fieldName]: event.target.value });
+    // }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+
     }
 
+    handleSubmit = (event) => {
+        console.log(this.state);
+        event.preventDefault();
+    }
+    
     render() {
         return (
             <div>
                 <h2>Create form</h2>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
-                        Name: <input type="text" onChange={(event) => this.handleChange(event, 'name')} />
+                        Name: <input type="text" name="name" onChange={this.handleChange} />
                     </div>
                     <div>
-                        Age: <input type="text" onChange={(event) => this.handleChange(event, 'age')} />
+                        Age: <input type="text" name="age" onChange={this.handleChange} />
+                    </div>
+                    <div>
+                        Color: <input type="text" name="color" onChange={this.handleChange} />
                     </div>
                     <button>Send me</button>
                 </form>
